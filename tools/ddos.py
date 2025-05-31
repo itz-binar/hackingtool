3a6d377a7b193283fb5d345318fece26dd5029da
Your Name <your.email@example.com>
2025-05-31 13:20:50 +0300

    Update ddos.py

--- tools/ddos.py
+++ tools/ddos.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import os
 import subprocess
+import shlex
 
 from core import HackingTool
 from core import HackingToolsCollection
@@ -16,7 +17,7 @@ class ddos(HackingTool):
 
     INSTALL_COMMANDS = [
         "git clone https://github.com/the-deepnet/ddos.git",
-        "cd ddos;sudo pip3 install -r requirements.txt",
+        "cd ddos && pip3 install -r requirements.txt",
     ]
     PROJECT_URL = "https://github.com/the-deepnet/ddos.git"
 
@@ -24,23 +25,21 @@ class ddos(HackingTool):
         method = input("Enter Method >> ")
         url = input("Enter URL >> ")
         threads = input("Enter Threads >> ")
-        proxylist = input(" Enter ProxyList >> ")
-        multiple = input(" Enter Multiple >> ")
-        timer = input(" Enter Timer >> ")
-        os.system("cd ddos;")
-        subprocess.run(
-            [
-                "sudo",
-                "python3 ddos",
-                method,
-                url,
-                "socks_type5.4.1",
-                threads,
-                proxylist,
-                multiple,
-                timer,
-            ]
-        )
+        proxylist = input("Enter ProxyList >> ")
+        multiple = input("Enter Multiple >> ")
+        timer = input("Enter Timer >> ")
+        
+        # Change to the ddos directory and run the command
+        current_dir = os.getcwd()
+        ddos_path = os.path.join(current_dir, "ddos")
+        
+        if os.path.exists(ddos_path):
+            os.chdir(ddos_path)
+            cmd = f"python3 ddos {method} {url} socks_type5.4.1 {threads} {proxylist} {multiple} {timer}"
+            os.system(cmd)
+            os.chdir(current_dir)  # Return to original directory
+        else:
+            print("\033[91mError: ddos directory not found. Please run the install command first.\033[0m")
 
 
 class SlowLoris(HackingTool):
@@ -49,7 +48,7 @@ class SlowLoris(HackingTool):
         "Slowloris is basically an HTTP Denial of Service attack."
         "It send lots of HTTP Request"
     )
-    INSTALL_COMMANDS = ["sudo pip3 install slowloris"]
+    INSTALL_COMMANDS = ["pip3 install slowloris"]
 
     def run(self):
         target_site = input("Enter Target Site:- ")
@@ -65,7 +64,7 @@ class Asyncrone(HackingTool):
     )
     INSTALL_COMMANDS = [
         "git clone https://github.com/fatih4842/aSYNcrone.git",
-        "cd aSYNcrone;sudo gcc aSYNcrone.c -o aSYNcrone -lpthread",
+        "cd aSYNcrone && gcc aSYNcrone.c -o aSYNcrone -lpthread",
     ]
     PROJECT_URL = "https://github.com/fatihsnsy/aSYNcrone"
 
@@ -73,10 +72,18 @@ class Asyncrone(HackingTool):
         source_port = input("Enter Source Port >> ")
         target_ip = input("Enter Target IP >> ")
         target_port = input("Enter Target port >> ")
-        os.system("cd aSYNcrone;")
-        subprocess.run(
-            ["sudo", "./aSYNcrone", source_port, target_ip, target_port, 1000]
-        )
+        
+        # Change to the aSYNcrone directory and run the command
+        current_dir = os.getcwd()
+        asyncrone_path = os.path.join(current_dir, "aSYNcrone")
+        
+        if os.path.exists(asyncrone_path):
+            os.chdir(asyncrone_path)
+            cmd = f"./aSYNcrone {source_port} {target_ip} {target_port} 1000"
+            os.system(cmd)
+            os.chdir(current_dir)  # Return to original directory
+        else:
+            print("\033[91mError: aSYNcrone directory not found. Please run the install command first.\033[0m")
 
 
 class UFONet(HackingTool):
@@ -88,10 +95,10 @@ class UFONet(HackingTool):
         "More Usage Visit"
     )
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/epsylon/ufonet.git",
-        "cd ufonet;sudo python3 setup.py install;sudo pip3 install GeoIP;sudo pip3 install python-geoip;sudo pip3 install pygeoip;sudo pip3 install requests;sudo pip3 install pycrypto;sudo pip3 install pycurl;sudo pip3 install whois;sudo pip3 install scapy-python3",
+        "git clone https://github.com/epsylon/ufonet.git",
+        "cd ufonet && python3 setup.py install && pip3 install GeoIP python-geoip pygeoip requests pycryptodome pycurl whois scapy-python3",
     ]
-    RUN_COMMANDS = ["sudo python3 ufonet --gui"]
+    RUN_COMMANDS = ["cd ufonet && python3 ufonet --gui"]
     PROJECT_URL = "https://github.com/epsylon/ufonet"
 
 
@@ -102,36 +109,51 @@ class GoldenEye(HackingTool):
         "GoldenEye is a HTTP DoS Test Tool."
     )
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/jseidl/GoldenEye.git;"
+        "git clone https://github.com/jseidl/GoldenEye.git",
         "chmod -R 755 GoldenEye"
     ]
     PROJECT_URL = "https://github.com/jseidl/GoldenEye"
 
     def run(self):
-        os.system("cd GoldenEye ;sudo ./goldeneye.py")
-        print("\033[96m Go to Directory \n [*] USAGE: ./goldeneye.py <url> [OPTIONS]")
+        current_dir = os.getcwd()
+        goldeneye_path = os.path.join(current_dir, "GoldenEye")
+        
+        if os.path.exists(goldeneye_path):
+            os.chdir(goldeneye_path)
+            os.system("./goldeneye.py")
+            print("\033[96m [*] USAGE: ./goldeneye.py <url> [OPTIONS]")
+            os.chdir(current_dir)  # Return to original directory
+        else:
+            print("\033[91mError: GoldenEye directory not found. Please run the install command first.\033[0m")
 
 
 class Saphyra(HackingTool):
     TITLE = "SaphyraDDoS"
     DESCRIPTION = "A complex python code to DDoS any website with a very easy usage.!\n"
     INSTALL_COMMANDS = [
-        "sudo su",
         "git clone https://github.com/anonymous24x7/Saphyra-DDoS.git",
-        "cd Saphyra-DDoS",
-        "chmod +x saphyra.py",
-        "python saphyra.py",
+        "cd Saphyra-DDoS && chmod +x saphyra.py"
     ]
     PROJECT_URL = "https://github.com/anonymous24x7/Saphyra-DDoS"
 
     def run(self):
         url = input("Enter url>>> ")
-        try:
-            os.system("python saphyra.py " + url)
-        except Exception:
-            print("Enter a valid url.")
+        
+        current_dir = os.getcwd()
+        saphyra_path = os.path.join(current_dir, "Saphyra-DDoS")
+        
+        if os.path.exists(saphyra_path):
+            os.chdir(saphyra_path)
+            try:
+                os.system(f"python3 saphyra.py {shlex.quote(url)}")
+            except Exception as e:
+                print(f"Error: {e}")
+                print("Enter a valid url.")
+            os.chdir(current_dir)  # Return to original directory
+        else:
+            print("\033[91mError: Saphyra-DDoS directory not found. Please run the install command first.\033[0m")
 
 
 class DDOSTools(HackingToolsCollection):
     TITLE = "DDOS Attack Tools"
-    TOOLS = [SlowLoris(), Asyncrone(), UFONet(), GoldenEye(), Saphyra()]
+    TOOLS = [ddos(), SlowLoris(), Asyncrone(), UFONet(), GoldenEye(), Saphyra()]
